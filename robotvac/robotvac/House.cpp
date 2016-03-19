c9294cd97b47fe4b81d6d7ab522f279d1b8b5679
guymenahem <guy.men14@gmail.com>
2016-03-19 17:45:39 +0200

    House

---
+++ robotvac/robotvac/House.cpp
@@ -0,0 +1,85 @@
+#include "House.h"
+#include "Conf.h"
+
+House::House(char _shortName[], char _longName[], int len, int wid, char ** house)
+	:length(len), width(wid)
+{
+	strcpy(shortName, _shortName);
+	strcpy(longName, _longName);
+	curHouse = new char*[wid];
+
+	// Init copy of house
+	for (int i = 0; i < wid; i++)
+	{
+		curHouse[i] = new char[len];
+		strcpy(curHouse[i], house[i]);
+	}
+
+	repairHouse();
+}
+
+
+SensorInformation House::Sense(Point location)
+{
+	SensorInformation info;
+
+	// Check if there is wall North
+	if (this->curHouse[location.getX()][location.getY() - 1] == 'W')
+		info.isWall[WallInfo::North] = true;
+
+	// South
+	if (this->curHouse[location.getX()][location.getY() + 1] == 'W')
+		info.isWall[WallInfo::South] = true;
+
+	// East
+	if (this->curHouse[location.getX() + 1][location.getY()] == 'W')
+		info.isWall[WallInfo::East] = true;
+
+	// West
+	if (this->curHouse[location.getX() - 1][location.getY()] == 'W')
+		info.isWall[WallInfo::East] = true;
+
+	info.dirtLevel = curHouse[location.getX()][location.getY()];
+
+	return info;
+}
+
+bool House::Clean(Point p)
+{
+	char dirt = curHouse[p.getX()][p.getY()]--;
+	return ((dirt - '0' > 0 && dirt - '0' <= 9));
+
+}
+
+void House::repairHouse()
+{
+	// put walls on Sides
+	for (int i = 0; i < this->width; i++)
+	{
+		this->curHouse[i][0] = 'W';
+		this->curHouse[i][this->length - 1] = 'W';
+	}
+		
+	// put Walls on Top & Bot
+	for (int i = 0; i < this->length; i++)
+	{
+		this->curHouse[0][i] = 'W';
+		this->curHouse[this->width - 1][i] = 'W';
+	}
+}
+
+bool House::isHouseClean()
+{
+	// Check on every block
+	for (int i = 0; i < this->width; i++)
+	{
+		for (int j = 0; j < this->length; j++)
+		{
+			if (this->curHouse[i][j]-'0' > 0 && this->curHouse[i][j]-'0' <= 9)
+				return false;
+		}
+	}
+
+	return true;
+}
+
